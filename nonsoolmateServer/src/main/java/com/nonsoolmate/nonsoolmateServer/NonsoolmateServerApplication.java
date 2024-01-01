1c9e7378cca13d87663b1fe3f01e3c74b6a08bd1
sung-silver <jinna0319@gmail.com>
2024-01-02 00:09:10 +0900

    [INIT] Project 생성

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/NonsoolmateServerApplication.java
@@ -0,0 +1,13 @@
+package com.nonsoolmate.nonsoolmateServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonsoolmateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonsoolmateServerApplication.class, args);
+	}
+
+}
---
+++ nonsoolmateServer/src/test/java/com/nonsoolmate/nonsoolmateServer/NonsoolmateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonsoolmate.nonsoolmateServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonsoolmateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
