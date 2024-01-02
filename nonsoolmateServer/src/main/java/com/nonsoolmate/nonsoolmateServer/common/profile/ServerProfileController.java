1b946897002a7573f27718e42d2ca2a9c2b31748
sung-silver <jinna0319@gmail.com>
2024-01-02 20:41:03 +0900

    [REFACTOR] getProfile 함수에서 env.getActiveProfiles()의 0번째 인덱스를 바로 반환하도록 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/common/profile/ServerProfileController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/common/profile/ServerProfileController.java
@@ -5,8 +5,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 @RestController
 @RequiredArgsConstructor
 public class ServerProfileController {
@@ -15,8 +13,6 @@ public class ServerProfileController {
 
     @GetMapping("/profile")
     public String getProfile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .findFirst()
-                .orElse("");
+        return env.getActiveProfiles()[0];
     }
 }
