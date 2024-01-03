f04d6724c282137bcf2d881b3d48b790ef7b11ec
sung-silver <jinna0319@gmail.com>
2024-01-03 13:25:41 +0900

    [ADD] Error Enum 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/error/Error.java
@@ -0,0 +1,29 @@
+package com.nonsoolmate.nonsoolmateServer.global.error;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Error {
+
+    // 400 BAD REQUEST
+    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+
+    // 404 NOT FOUND
+
+    // 500 INTERNAL SERVER ERROR
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatus(){
+        return httpStatus.value();
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+}
