b7da97e796e3c293b406b0c9aed16f8cda5408db
sung-silver <jinna0319@gmail.com>
2024-01-04 02:49:57 +0900

    [FEAT] Member 엔티티 개발

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/domain/Member.java
@@ -0,0 +1,53 @@
+package com.nonsoolmate.nonsoolmateServer.domain.member.domain;
+
+import com.nonsoolmate.nonsoolmateServer.global.common.enums.PlatformType;
+import com.nonsoolmate.nonsoolmateServer.global.common.enums.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.Year;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String memberName;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private PlatformType platformType;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Role role;
+
+    @NotNull
+    private Year memberBirthYear;
+
+    @ColumnDefault("0")
+    private int ticketCount;
+
+    private LocalDateTime ticketPreviousPublicationTime;
+
+    @NotNull
+    private String phoneNumber;
+}
