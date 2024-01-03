2fb7fe39a1b16838a16aed62bcc39c604b68a86b
sung-silver <jinna0319@gmail.com>
2024-01-04 02:51:38 +0900

    [FEAT] University 엔티티 개발

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/domain/University.java
@@ -0,0 +1,27 @@
+package com.nonsoolmate.nonsoolmateServer.domain.university.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long universityId;
+
+    @NotNull
+    private String universityName;
+
+    @NotNull
+    private String universityCollege;
+}
