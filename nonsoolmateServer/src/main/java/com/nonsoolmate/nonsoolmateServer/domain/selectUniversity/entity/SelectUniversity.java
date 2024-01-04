34b3c78e1dc888c3f0d8fa790d3967814dbaf58b
sung-silver <jinna0319@gmail.com>
2024-01-05 01:14:30 +0900

    [FEAT] selectUniversity 구현

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/entity/SelectUniversity.java
@@ -0,0 +1,32 @@
+package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.entity;
+
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+import com.nonsoolmate.nonsoolmateServer.domain.university.entity.University;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SelectUniversity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long selectUniversityId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    private University university;
+}
