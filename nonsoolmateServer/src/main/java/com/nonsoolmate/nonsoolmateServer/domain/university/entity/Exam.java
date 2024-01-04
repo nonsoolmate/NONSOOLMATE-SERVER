249ea79bf92bd570b2c59125bc9a0981542f19fc
sung-silver <jinna0319@gmail.com>
2024-01-04 22:22:52 +0900

    [FIX] Exam 엔티티 위치 변경

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/entity/Exam.java
@@ -0,0 +1,43 @@
+package com.nonsoolmate.nonsoolmateServer.domain.university.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.sql.Time;
+import java.time.Year;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Exam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long examId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "university_id")
+    private University university;
+
+    @NotNull
+    private String examName;
+
+    @NotNull
+    private String examAnswerUrl;
+
+    @NotNull
+    private Year examYear;
+
+    @NotNull
+    private Time examTimeLimit;
+}
