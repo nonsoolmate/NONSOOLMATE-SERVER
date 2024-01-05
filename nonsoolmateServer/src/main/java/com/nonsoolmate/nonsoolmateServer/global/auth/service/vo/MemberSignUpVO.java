cb190c31bfbdfd76fe0f2cb97a8148c321c28a3b
sung-silver <jinna0319@gmail.com>
2024-01-06 06:15:22 +0900

    [ADD] MemberSignUpVO 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/auth/service/vo/MemberSignUpVO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/auth/service/vo/MemberSignUpVO.java
@@ -1,10 +1,29 @@
 package com.nonsoolmate.nonsoolmateServer.global.auth.service.vo;
 
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.PlatformType;
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.Role;
+import com.nonsoolmate.nonsoolmateServer.global.auth.service.vo.enums.AuthType;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class MemberSignUpVO {
-    private final Long memberId;
-    private final String email;
-    private final String memberName;
+@Builder
+@Getter
+public record MemberSignUpVO(Long memberId, String email, String name, PlatformType platformType, Role role,
+                             String birthYear, String gender, String phoneNumber, AuthType authType) {
+    public static MemberSignUpVO of(Member member, PlatformType platformType, AuthType authtype) {
+        return MemberSignUpVO.builder()
+                .memberId(member.getMemberId())
+                .email(member.getEmail())
+                .name(member.getName())
+                .platformType(platformType)
+                .role(Role.USER)
+                .birthYear(member.getBirthYear())
+                .gender(member.getGender())
+                .phoneNumber(member.getPhoneNumber())
+                .authType(authtype)
+                .build();
+    }
 }
