a7870342db9236db6981cee3ea7a366d1c91426d
mikekks <thdalsrb79@naver.com>
2024-01-07 16:35:38 +0900

    [ADD] CORS 설정 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.nonsoolmate.nonsoolmateServer.global.config;
+
+import com.nonsoolmate.nonsoolmateServer.domain.member.repository.MemberRepository;
+import com.nonsoolmate.nonsoolmateServer.global.auth.jwt.service.JwtService;
+import io.swagger.v3.oas.models.PathItem.HttpMethod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/", "/error", "/webjars/**",
+
+            // Swagger
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/webjars/**",
+
+            // Authentication
+            "/login/**",
+            "/auth/**",
+            "/oauth2/**",
+            "/api/v1/auth/**", // 자체 로그인 요청
+            "/api/v1/oauth2/**", // OAuth 소셜 로그인 요청
+            "/login",
+
+            // client
+            "/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**"
+    };
+
+
+    //cors 에러를 대응하기 위한 메소드
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://nonsoolmate.com", "localhost:3000")
+                        .allowedOriginPatterns("https://nonsoolmate.com", "localhost:3000")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.PATCH.name()
+                        );
+            }
+        };
+    }
+
+
+
+}
