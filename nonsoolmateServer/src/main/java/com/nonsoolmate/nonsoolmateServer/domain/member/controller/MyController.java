f375f23742fbf34ae27a30c2b3af3d843aae45d0
sung-silver <jinna0319@gmail.com>
2024-01-09 01:48:50 +0900

    [REFACTOR] nickname -> name 네이밍 리팩터링

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/MyController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/MyController.java
@@ -1,6 +1,6 @@
 package com.nonsoolmate.nonsoolmateServer.domain.member.controller;
 
-import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.nicknameResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.NameResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.member.exception.MemberSuccessType;
 import com.nonsoolmate.nonsoolmateServer.domain.member.service.MemberService;
 import com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse;
@@ -17,7 +17,7 @@ public class MyController {
     private final MemberService memberService;
 
     @GetMapping("/name")
-    public ResponseEntity<ApiResponse<nicknameResponseDTO>> getName() {
+    public ResponseEntity<ApiResponse<NameResponseDTO>> getName() {
         return ResponseEntity.ok()
                 .body(ApiResponse.success(MemberSuccessType.GET_MEMBER_NAME_SUCCESS, memberService.getNickname()));
     }
