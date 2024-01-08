8086500713879a2eae6f5f68feef6643e87076d4
mikekks <thdalsrb79@naver.com>
2024-01-08 22:23:46 +0900

    [FEAT] Jwt 필터 구현

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/jwt/service/JwtTokenProvider.java
@@ -0,0 +1,85 @@
+package com.nonsoolmate.nonsoolmateServer.global.auth.jwt.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String EMAIL_CLAIM = "email";
+    private static final String MEMBER_ID_CLAIM = "memberId";
+
+
+    private final Key key;
+    private final ObjectMapper objectMapper;
+
+    public JwtTokenProvider(@Value("${jwt.secretKey}") String secretKey, ObjectMapper objectMapper) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.objectMapper = objectMapper;
+    }
+
+    public String createAccessToken(String email, Long memberId, Long expirationTime) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(ACCESS_TOKEN_SUBJECT)
+                .claim(MEMBER_ID_CLAIM, memberId)
+                .claim(EMAIL_CLAIM, email)
+                .setIssuedAt(now)   //토큰 발행 시간 정보
+                .setExpiration(new Date(now.getTime() + expirationTime))  //토큰 만료 시간 설정
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createRefreshToken(Long expirationTime) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(REFRESH_TOKEN_SUBJECT)
+                .setIssuedAt(now)   //토큰 발행 시간 정보
+                .setExpiration(new Date(now.getTime() + expirationTime))  //토큰 만료 시간 설정
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public Claims getTokenClaims(final String token) {
+        JwtParser build = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Claims makeInfoToClaim(String infoName, final Long memberId) throws JsonProcessingException {
+        String claimValue = objectMapper.writeValueAsString(memberId);
+        Claims claims = Jwts.claims();
+        claims.put(infoName, claimValue);
+        return claims;
+    }
+
+    public Long getMemberIdFromClaim(Claims claims, String infoName) throws JsonProcessingException {
+
+        return claims.get(infoName, Long.class);
+    }
+}
