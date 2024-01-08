e7845adbc0fac222325e4a5a3897444e71641ef1
mikekks <thdalsrb79@naver.com>
2024-01-08 22:21:30 +0900

    [ADD] SecurityConfig 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/security/config/SecurityConfig.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/security/config/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.nonsoolmate.nonsoolmateServer.global.security.config;
 
+import com.nonsoolmate.nonsoolmateServer.global.security.filter.JwtAuthenticationFilter;
 import io.swagger.v3.oas.models.PathItem.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,11 +31,13 @@ public class SecurityConfig {
             "/webjars/**",
 
             // Authentication
-            "/auth/**",
+            "/auth/**", "/login/**","/authTest",
 
             // client
-            "/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**"
+            "/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**"
     };
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
     //cors 에러를 대응하기 위한 메소드
@@ -47,6 +57,41 @@ public class SecurityConfig {
                         );
             }
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(frameOptionsConfig ->
+                                frameOptionsConfig.disable()
+                        )
+                )
+              //  .userDetailsService(memberAuthService)
+        ;
+
+        //== URL별 권한 관리 옵션 ==//
+        http.authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(AUTH_WHITELIST).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                // 원래 스프링 시큐리티 필터 순서가 LogoutFilter 이후에 로그인 필터 동작
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 
