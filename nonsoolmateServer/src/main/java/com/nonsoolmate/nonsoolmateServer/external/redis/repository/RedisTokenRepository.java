85fb0ab55cf6ebc6bd2c7d8a625f4b5a62ca6305
mikekks <thdalsrb79@naver.com>
2024-01-08 22:22:42 +0900

    [FEAT] redis config 및 repository 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/redis/repository/RedisTokenRepository.java
@@ -0,0 +1,24 @@
+package com.nonsoolmate.nonsoolmateServer.external.redis.repository;
+
+
+import static com.nonsoolmate.nonsoolmateServer.domain.auth.exception.AuthExceptionType.NOT_FOUND_REFRESH_TOKEN;
+
+import com.nonsoolmate.nonsoolmateServer.domain.auth.exception.AuthException;
+
+
+import com.nonsoolmate.nonsoolmateServer.global.jwt.service.vo.RefreshTokenVO;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RedisTokenRepository extends CrudRepository<RefreshTokenVO, String> {
+    Optional<RefreshTokenVO> findByMemberId(String memberId);
+
+    default RefreshTokenVO findByMemberIdOrElseThrowException(String memberId) {
+        return findByMemberId(memberId)
+                .filter(refreshTokenVO -> !refreshTokenVO.isBlack())
+                .orElseThrow(
+                () -> new AuthException(NOT_FOUND_REFRESH_TOKEN));
+    }
+}
