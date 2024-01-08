79617e8efc9a4d96f68f0c45b91ef723f8ac5092
sung-silver <jinna0319@gmail.com>
2024-01-09 01:48:50 +0900

    [REFACTOR] nickname -> name 네이밍 리팩터링

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/nicknameResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response;
-
-public record nicknameResponseDTO(String memberName) {
-    static public nicknameResponseDTO of(String memberName) {
-        return new nicknameResponseDTO(memberName);
-    }
-}
--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/service/MemberService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/service/MemberService.java
@@ -1,11 +1,15 @@
 package com.nonsoolmate.nonsoolmateServer.domain.member.service;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.NameResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.TicketResponseDTO;
 =======
 import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.nicknameResponseDTO;
 >>>>>>> 6409e08 ([FEAT] MemberService 내 getName() 구현)
+=======
+import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.NameResponseDTO;
+>>>>>>> 0660a64 ([REFACTOR] nickname -> name 네이밍 리팩터링)
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.member.repository.MemberRepository;
 import com.nonsoolmate.nonsoolmateServer.global.security.CustomAuthUser;
