caa35f48f6d8ca5bf3d6255a914dcd6bfe7d883e
sung-silver <jinna0319@gmail.com>
2024-01-09 23:36:06 +0900

    [FIX] ExamResultStatus 상수 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/entity/enums/ExamResultStatus.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/entity/enums/ExamResultStatus.java
@@ -1,5 +1,12 @@
 package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum ExamResultStatus {
-    ONGOING, FINISH
+    ONGOING("첨삭 진행 중"), FINISH("첨삭 완료");
+
+    private final String status;
 }
