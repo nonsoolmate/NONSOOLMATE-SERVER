3f7287e406bdec783ec36cacaa4b286d25e317c4
sung-silver <jinna0319@gmail.com>
2024-01-09 23:35:30 +0900

    [ADD] universityCollege 필드 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/response/SelectUniversityExamsResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/response/SelectUniversityExamsResponseDTO.java
@@ -2,10 +2,11 @@ package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto
 
 import java.util.List;
 
-public record SelectUniversityExamsResponseDTO(Long universityId, String universityName,
+public record SelectUniversityExamsResponseDTO(Long universityId, String universityName, String universityCollege,
                                                List<SelectUniversityExamResponseDTO> examList) {
     public static SelectUniversityExamsResponseDTO of(Long universityId, String universityName,
+                                                      String universityCollege,
                                                       List<SelectUniversityExamResponseDTO> examList) {
-        return new SelectUniversityExamsResponseDTO(universityId, universityName, examList);
+        return new SelectUniversityExamsResponseDTO(universityId, universityName, universityCollege, examList);
     }
 }
