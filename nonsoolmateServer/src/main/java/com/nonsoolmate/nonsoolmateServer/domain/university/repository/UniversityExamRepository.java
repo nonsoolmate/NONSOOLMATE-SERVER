48b21cc921dd162e4884d866875c9e90c4e62471
sung-silver <jinna0319@gmail.com>
2024-01-09 23:39:07 +0900

    [ADD] UniversityExamrepository, UniversityExamRecordRepository 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/UniversityExamRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/UniversityExamRepository.java
@@ -1,9 +1,13 @@
 package com.nonsoolmate.nonsoolmateServer.domain.university.repository;
 
+import com.nonsoolmate.nonsoolmateServer.domain.university.entity.University;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UniversityExamRepository extends JpaRepository<UniversityExam, Long> {
     Optional<UniversityExam> findByUniversityExamId(Long universityId);
+
+    List<UniversityExam> findAllByUniversity(University university);
 }
