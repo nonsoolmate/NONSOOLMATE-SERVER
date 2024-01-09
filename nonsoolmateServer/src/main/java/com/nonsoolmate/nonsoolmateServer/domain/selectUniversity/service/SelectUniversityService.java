c20069afa2bfd1592f0e76fac57b33320d6a2e78
sung-silver <jinna0319@gmail.com>
2024-01-09 23:39:52 +0900

    [FEAT] SelectUniversityService getSelectUniversityExams() 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/service/SelectUniversityService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/service/SelectUniversityService.java
@@ -1,11 +1,16 @@
 package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.service;
 
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response.SelectUniversityExamResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response.SelectUniversityExamsResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response.SelectUniversityResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.entity.SelectUniversity;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.repository.SelectUniversityRepository;
-import com.nonsoolmate.nonsoolmateServer.domain.university.entity.University;
+import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam;
+import com.nonsoolmate.nonsoolmateServer.domain.university.repository.UniversityExamRepository;
 import com.nonsoolmate.nonsoolmateServer.domain.university.repository.UniversityRepository;
+import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.entity.UniversityExamRecord;
+import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.repository.UniversityExamRecordRepository;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +23,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class SelectUniversityService {
     private final SelectUniversityRepository selectUniversityRepository;
     private final UniversityRepository universityRepository;
+    private final UniversityExamRepository universityExamRepository;
+    private final UniversityExamRecordRepository universityExamRecordRepository;
 
 
-    public List<SelectUniversityResponseDTO> getSelectUniversities(Member member){
+    public List<SelectUniversityResponseDTO> getSelectUniversities(Member member) {
         List<SelectUniversity> selectUniversities = selectUniversityRepository.findAllByMember(member);
         List<SelectUniversityResponseDTO> selectUniversityResponseDTOS = new ArrayList<>();
 
         universityRepository.findAll().stream().forEach(university -> {
             boolean status = false;
 
-            for(SelectUniversity selectUniversity : selectUniversities){
-                if(university.getUniversityId() == selectUniversity.getUniversity().getUniversityId()){
+            for (SelectUniversity selectUniversity : selectUniversities) {
+                if (university.getUniversityId() == selectUniversity.getUniversity().getUniversityId()) {
                     status = true;
                 }
             }
@@ -38,5 +45,46 @@ public class SelectUniversityService {
         });
 
         return selectUniversityResponseDTOS;
+    }
+
+    public List<SelectUniversityExamsResponseDTO> getSelectUniversityExams(Member member) {
+        List<SelectUniversityExamsResponseDTO> selectUniversityExamsResponseDTOS = new ArrayList<>();
+        List<SelectUniversity> selectUniversities = selectUniversityRepository.findAllByMember(member);
+
+        for (SelectUniversity selectUniversity : selectUniversities) {
+            selectUniversityExamsResponseDTOS.add(getSelectUniversityExamsResponseDTO(selectUniversity, member));
+        }
+
+        return selectUniversityExamsResponseDTOS;
+    }
+
+    private SelectUniversityExamsResponseDTO getSelectUniversityExamsResponseDTO(SelectUniversity selectUniversity,
+                                                                                 Member member) {
+        List<SelectUniversityExamResponseDTO> selectUniversityExamResponseDTOS = new ArrayList<>();
+
+        List<UniversityExam> universityExams = universityExamRepository.findAllByUniversity(
+                selectUniversity.getUniversity());
+
+        selectUniversityExamResponseDTOS = getSelectUniversityExamResponseDTOS(universityExams, member);
+
+        return SelectUniversityExamsResponseDTO.of(selectUniversity.getUniversity().getUniversityId(),
+                selectUniversity.getUniversity().getUniversityName(),
+                selectUniversity.getUniversity().getUniversityCollege(), selectUniversityExamResponseDTOS);
+    }
+
+    private List<SelectUniversityExamResponseDTO> getSelectUniversityExamResponseDTOS(
+            List<UniversityExam> universityExams, Member member) {
+        UniversityExamRecord universityExamRecord;
+        List<SelectUniversityExamResponseDTO> selectUniversityExamResponseDTOS = new ArrayList<>();
+        for (UniversityExam universityExam : universityExams) {
+            universityExamRecord = universityExamRecordRepository.findByUniversityExamAndMember(universityExam, member)
+                    .orElse(null);
+            String status =
+                    universityExamRecord == null ? "시험 응시 전" : universityExamRecord.getExamResultStatus().getStatus();
+            selectUniversityExamResponseDTOS.add(
+                    SelectUniversityExamResponseDTO.of(universityExam.getUniversityExamId(),
+                            universityExam.getExamName(), universityExam.getExamTimeLimit(), status));
+        }
+        return selectUniversityExamResponseDTOS;
     }
 }
