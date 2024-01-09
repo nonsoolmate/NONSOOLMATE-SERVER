6372d971d06115d51fa5f38e9206cc4a68b0f05c
sung-silver <jinna0319@gmail.com>
2024-01-09 16:59:24 +0900

    [FEAT] UniversityExamService getUniversityExamImages() 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/service/UniversityExamService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/service/UniversityExamService.java
@@ -1,10 +1,15 @@
 package com.nonsoolmate.nonsoolmateServer.domain.university.service;
 
+import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamImageResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamInfoResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam;
+import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExamImage;
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamException;
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamExceptionType;
+import com.nonsoolmate.nonsoolmateServer.domain.university.repository.UniversityExamImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.nonsoolmate.nonsoolmateServer.domain.university.repository.UniversityExamRepository;
@@ -14,6 +19,7 @@ import com.nonsoolmate.nonsoolmateServer.domain.university.repository.University
 @RequiredArgsConstructor
 public class UniversityExamService {
     private final UniversityExamRepository universityExamRepository;
+    private final UniversityExamImageRepository universityExamImageRepository;
 
     public UniversityExamInfoResponseDTO getUniversityExam(Long universityExamId) {
         UniversityExam universityExam = universityExamRepository.findByUniversityExamId(universityExamId)
@@ -21,6 +27,16 @@ public class UniversityExamService {
                         UniversityExamExceptionType.NOT_FOUND_UNIVERSITY_EXAM));
         return UniversityExamInfoResponseDTO.of(universityExam.getUniversityExamId(), universityExam.getExamName(),
                 universityExam.getExamTimeLimit());
+    }
+
+    public Page<UniversityExamImageResponseDTO> getUniversityExamImages(Long id, Pageable pageable) {
+        UniversityExam universityExam = universityExamRepository.findByUniversityExamId(id)
+                .orElseThrow(() -> new UniversityExamException(
+                        UniversityExamExceptionType.NOT_FOUND_UNIVERSITY_EXAM));
+        Page<UniversityExamImage> universityExamImages = universityExamImageRepository.findAllByUniversityExam(
+                universityExam,
+                pageable);
+        return universityExamImages.map(image -> UniversityExamImageResponseDTO.of(image.getUniversityExamImageUrl()));
     }
 
 }
