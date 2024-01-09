a0df6a3c4e9d16e15c53f0edd20759ce866b08f6
sung-silver <jinna0319@gmail.com>
2024-01-09 17:00:17 +0900

    [FEAT] UniversityExamController getUniversityExamImages() 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/UniversityExamController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/UniversityExamController.java
@@ -1,14 +1,19 @@
 package com.nonsoolmate.nonsoolmateServer.domain.university.controller;
 
+import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamImageResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamInfoResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamSuccessType;
 import com.nonsoolmate.nonsoolmateServer.domain.university.service.UniversityExamService;
 import com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +27,14 @@ public class UniversityExamController {
             @PathVariable("id") Long universityExamId) {
         return ResponseEntity.ok().body(ApiResponse.success(UniversityExamSuccessType.GET_UNIVERSITY_EXAM_SUCCESS,
                 universityExamService.getUniversityExam(universityExamId)));
+    }
+
+    @GetMapping("{id}/image")
+    public ResponseEntity<ApiResponse<Page<UniversityExamImageResponseDTO>>> getUniversityExamImages(
+            @PathVariable("id") Long id, @RequestParam(defaultValue = "0") int page, Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(page, 1);
+        Page<UniversityExamImageResponseDTO> images = universityExamService.getUniversityExamImages(id, pageRequest);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(UniversityExamSuccessType.GET_UNIVERSITY_EXAM_IMAGE_SUCCESS, images));
     }
 }
