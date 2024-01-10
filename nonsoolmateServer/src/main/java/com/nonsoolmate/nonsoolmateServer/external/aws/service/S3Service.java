6e24cf5a09f1eaa5343983c63113d43f2e4988c2
mikekks <thdalsrb79@naver.com>
2024-01-11 06:23:53 +0900

    [ADD] cloudfront 객체 코드 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/S3Service.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/S3Service.java
@@ -1,15 +1,20 @@
 package com.nonsoolmate.nonsoolmateServer.external.aws.service;
 
+import com.amazonaws.services.cloudfront.CloudFrontUrlSigner;
+import com.amazonaws.services.cloudfront.util.SignerUtils;
 import com.nonsoolmate.nonsoolmateServer.external.aws.config.AWSConfig;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.spec.InvalidKeySpecException;
 import java.time.Duration;
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+import org.springframework.util.ResourceUtils;
+
 
 @Component
 @RequiredArgsConstructor
@@ -20,29 +25,68 @@ public class S3Service {
     private String bucketName;
     private final AWSConfig awsConfig;
 
+    @Value("${aws-property.distribution-domain}")
+    private String distributionDomain;
+
+    @Value("${aws-property.private-key-file-path}")
+    private String privateKeyFilePath;
+
+    @Value("${aws-property.key-pair-id}")
+    private String keyPairId;
+
     private static final Long PRE_SIGNED_URL_EXPIRE_MINUTE = 1L;
 
     public String createPresignedGetUrl(String path, String fileName) {
         final String key = path + fileName;
+        String s3ObjectKey = key; // S3 객체 키
 
-        try (S3Presigner presigner = S3Presigner.create()) {
+        // 현재 시간에서 1시간 유효한 URL 생성
+        Duration duration = Duration.ofHours(1);
+        Date expirationTime = new Date(System.currentTimeMillis() + duration.toMillis());
+        File privateKeyFile = null;
+        String signedUrl = null;
+        SignerUtils.Protocol protocol = SignerUtils.Protocol.https;
+        try {
+            privateKeyFile = ResourceUtils.getFile(privateKeyFilePath);
 
-            GetObjectRequest objectRequest = GetObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(key)
-                    .build();
+            signedUrl = CloudFrontUrlSigner.getSignedURLWithCannedPolicy(
+                    protocol,
+                    distributionDomain,
+                    privateKeyFile,
+                    s3ObjectKey,
+                    keyPairId,
+                    expirationTime
+            );
 
-            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                    .signatureDuration(Duration.ofMinutes(PRE_SIGNED_URL_EXPIRE_MINUTE))  // The URL will expire in 10 minutes.
-                    .getObjectRequest(objectRequest)
-                    .build();
+            System.out.println("Signed URL: " + signedUrl);
+        }catch (FileNotFoundException e){
+            log.info("FileNotFoundException = {}", e);
+        }catch (IOException e){
+            log.info("");
+        }catch (InvalidKeySpecException e){
 
-            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
-            log.info("Presigned URL: [{}]", presignedRequest.url().toString());
-            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
-
-            return presignedRequest.url().toExternalForm();
         }
+        return signedUrl;
+
+//
+//        try (S3Presigner presigner = S3Presigner.create()) {
+//
+//            GetObjectRequest objectRequest = GetObjectRequest.builder()
+//                    .bucket(bucketName)
+//                    .key(key)
+//                    .build();
+//
+//            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+//                    .signatureDuration(Duration.ofMinutes(PRE_SIGNED_URL_EXPIRE_MINUTE))  // The URL will expire in 10 minutes.
+//                    .getObjectRequest(objectRequest)
+//                    .build();
+//
+//            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+//            log.info("Presigned URL: [{}]", presignedRequest.url().toString());
+//            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
+//
+//            return presignedRequest.url().toExternalForm();
+//        }
     }
 
 }
