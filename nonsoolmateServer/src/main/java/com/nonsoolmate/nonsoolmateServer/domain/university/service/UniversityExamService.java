306a54c417fe2f237df9bce2ce7af9285e116c38
mikekks <thdalsrb79@naver.com>
2024-01-11 02:01:19 +0900

    [FEAT] 문제 이미지 및 해제 PDF presignedUrl 생성 로직 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/service/UniversityExamService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/service/UniversityExamService.java
@@ -1,5 +1,6 @@
 package com.nonsoolmate.nonsoolmateServer.domain.university.service;
 
+import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamImageAndAnswerResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamImageResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.UniversityExamInfoResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam;
@@ -7,6 +8,9 @@ import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamException;
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamExceptionType;
 import com.nonsoolmate.nonsoolmateServer.domain.university.repository.UniversityExamImageRepository;
+import com.nonsoolmate.nonsoolmateServer.external.aws.service.S3Service;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,8 +22,14 @@ import com.nonsoolmate.nonsoolmateServer.domain.university.repository.University
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UniversityExamService {
+    private static final String EXAM_ANSWER_PATH = "exam-answer/";
+    private static final String EXAM_IMAGE_PATH = "exam-image/";
+
+
     private final UniversityExamRepository universityExamRepository;
     private final UniversityExamImageRepository universityExamImageRepository;
+    private final S3Service s3Service;
+
 
     public UniversityExamInfoResponseDTO getUniversityExam(Long universityExamId) {
         UniversityExam universityExam = universityExamRepository.findByUniversityExamId(universityExamId)
@@ -36,7 +46,33 @@ public class UniversityExamService {
         Page<UniversityExamImage> universityExamImages = universityExamImageRepository.findAllByUniversityExam(
                 universityExam,
                 pageable);
-        return universityExamImages.map(image -> UniversityExamImageResponseDTO.of(image.getUniversityExamImageUrl()));
+        return universityExamImages.map(
+                image -> UniversityExamImageResponseDTO.of(image.getUniversityExamImageFileName()));
+    }
+
+    public UniversityExamImageAndAnswerResponseDTO getUniversityExamImageAndAnswer(Long universityExamId) {
+        UniversityExam universityExam = universityExamRepository.findByUniversityExamId(universityExamId)
+                .orElseThrow(() -> new UniversityExamException(
+                        UniversityExamExceptionType.NOT_FOUND_UNIVERSITY_EXAM));
+
+        String examAnswerUrl = s3Service.createPresignedGetUrl(EXAM_ANSWER_PATH,
+                universityExam.getExamAnswerFileName());
+        List<UniversityExamImageResponseDTO> examImageUrls = new ArrayList<>();
+
+        List<UniversityExamImage> UniversityExamImages = universityExamImageRepository.findAllByUniversityExam(
+                universityExam);
+
+        UniversityExamImages.stream().forEach(universityExamImage -> {
+            String presignedGetUrl = s3Service.createPresignedGetUrl(EXAM_IMAGE_PATH,
+                    universityExamImage.getUniversityExamImageFileName());
+
+            examImageUrls.add(
+                    UniversityExamImageResponseDTO.of(presignedGetUrl));
+        });
+
+        return UniversityExamImageAndAnswerResponseDTO.of(
+                universityExam.getUniversity().getUniversityName(),
+                universityExam.getUniversity().getUniversityCollege(), examImageUrls, examAnswerUrl);
     }
 
 }
