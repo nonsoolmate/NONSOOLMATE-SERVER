700e7a3371cd0dd9bd4ded25c9c205f12e0da02c
mikekks <thdalsrb79@naver.com>
2024-01-11 01:59:47 +0900

    [ADD] 문제 이미지 리스트 및 해제 PDF url 반환 DTO 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/UniversityExamImageAndAnswerResponseDTO.java
@@ -0,0 +1,14 @@
+package com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto;
+
+import java.util.List;
+
+public record UniversityExamImageAndAnswerResponseDTO(
+        String university, String college, List<UniversityExamImageResponseDTO> examQuestionList, String examAnswerUrl
+) {
+    static public UniversityExamImageAndAnswerResponseDTO of(String university, String college,
+                                                             List<UniversityExamImageResponseDTO> examQuestionList,
+                                                             String examAnswerUrl) {
+        return new UniversityExamImageAndAnswerResponseDTO(university, college, examQuestionList, examAnswerUrl);
+    }
+}
+
