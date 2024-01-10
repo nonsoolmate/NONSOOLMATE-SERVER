11a9119ad0b5213975547eec0622d640d0c5d151
mikekks <thdalsrb79@naver.com>
2024-01-11 02:00:26 +0900

    [FEAT] 대학 시험 조회 메서드 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/UniversityExamImageRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/UniversityExamImageRepository.java
@@ -2,10 +2,15 @@ package com.nonsoolmate.nonsoolmateServer.domain.university.repository;
 
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExamImage;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UniversityExamImageRepository extends JpaRepository<UniversityExamImage, Long> {
     Page<UniversityExamImage> findAllByUniversityExam(UniversityExam universityExam, Pageable pageable);
+
+    List<UniversityExamImage> findAllByUniversityExam(UniversityExam universityExam);
+
 }
