07a9106a3d4fc241563f23b43278bc790bc8611e
mikekks <thdalsrb79@naver.com>
2024-01-10 10:13:01 +0900

    [FEAT] 목표 대학 삭제 메서드 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/repository/SelectUniversityRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/repository/SelectUniversityRepository.java
@@ -3,11 +3,16 @@ package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.repository;
 
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.entity.SelectUniversity;
+import com.nonsoolmate.nonsoolmateServer.domain.university.entity.University;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SelectUniversityRepository extends JpaRepository<SelectUniversity, Long> {
 
     List<SelectUniversity> findAllByMember(Member member);
+
+    @Transactional
+    void deleteByMemberAndUniversity(Member member, University university);
 
 }
