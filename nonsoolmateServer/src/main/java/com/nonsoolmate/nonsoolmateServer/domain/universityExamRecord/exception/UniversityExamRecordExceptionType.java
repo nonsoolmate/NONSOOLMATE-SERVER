b6f8cd4c8d29a90151ac8bd697bdad28d874ee44
mikekks <thdalsrb79@naver.com>
2024-01-11 00:02:33 +0900

    [ADD] UniversityExamRecord 예외 enum 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/exception/UniversityExamRecordExceptionType.java
@@ -0,0 +1,24 @@
+package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.exception;
+
+import com.nonsoolmate.nonsoolmateServer.global.error.exception.BusinessExceptionType;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum UniversityExamRecordExceptionType implements BusinessExceptionType {
+    NOT_FOUND_UNIVERSITY_EXAM_RECORD(HttpStatus.NOT_FOUND, "존재하지 않는 시험 응시 기록입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus status() {
+        return this.status;
+    }
+
+    @Override
+    public String message() {
+        return this.message;
+    }
+}
