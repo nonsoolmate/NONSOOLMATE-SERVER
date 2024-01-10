17a2e75d2175c84a187af866c57f1a35f5796b4d
mikekks <thdalsrb79@naver.com>
2024-01-10 23:55:20 +0900

    [FEAT] presignedUrl 방식의 조회 메서드 구현

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/S3Service.java
@@ -0,0 +1,49 @@
+package com.nonsoolmate.nonsoolmateServer.external.aws.service;
+
+import com.nonsoolmate.nonsoolmateServer.external.aws.config.AWSConfig;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class S3Service {
+
+    @Value("${aws-property.s3-bucket-name}")
+    private String bucketName;
+    private final AWSConfig awsConfig;
+
+    // 만료시간 1분
+    private static final Long PRE_SIGNED_URL_EXPIRE_MINUTE = 60L;
+
+    public String createPresignedGetUrl(String path, String fileName) {
+        final String key = path + fileName;
+
+        try (S3Presigner presigner = S3Presigner.create()) {
+
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(PRE_SIGNED_URL_EXPIRE_MINUTE))  // The URL will expire in 10 minutes.
+                    .getObjectRequest(objectRequest)
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+            log.info("Presigned URL: [{}]", presignedRequest.url().toString());
+            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
+
+            return presignedRequest.url().toExternalForm();
+        }
+    }
+
+}
