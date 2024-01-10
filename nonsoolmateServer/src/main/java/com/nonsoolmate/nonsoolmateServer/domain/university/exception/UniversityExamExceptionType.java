808502bb6cd27d0db7037c9180ee87a257bc2671
mikekks <thdalsrb79@naver.com>
2024-01-11 01:58:39 +0900

    [ADD] UniversityExamExceptionType 메세지 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/exception/UniversityExamExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/exception/UniversityExamExceptionType.java
@@ -11,7 +11,8 @@ public enum UniversityExamExceptionType implements BusinessExceptionType {
     /**
      * 404 Not Found
      */
-    NOT_FOUND_UNIVERSITY_EXAM(HttpStatus.NOT_FOUND, "존재하지 않는 대학 시험입니다.");
+    NOT_FOUND_UNIVERSITY_EXAM(HttpStatus.NOT_FOUND, "존재하지 않는 대학 시험입니다."),
+    NOT_FOUND_UNIVERSITY_EXAM_IMAGE(HttpStatus.NOT_FOUND, "존재하지 않는 대학 시험 입니다.");
 
     private final HttpStatus status;
     private final String message;
