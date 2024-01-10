51e185e391961a4380838ea48a2b94bc465424b0
mikekks <thdalsrb79@naver.com>
2024-01-10 10:12:37 +0900

    [FEAT] 목표 대학 수정 controller단 메서드 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/SelectUniversityController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/SelectUniversityController.java
@@ -2,17 +2,23 @@ package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller;
 
 import static com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.exception.SelectUniversitySuccessType.GET_SELECT_UNIVERSITIES_SUCCESS;
 import static com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.exception.SelectUniversitySuccessType.GET_SELECT_UNIVERSITY_EXAMS_SUCCESS;
+import static com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.exception.SelectUniversitySuccessType.PATCH_SELECT_UNIVERSITIES_SUCCESS;
 
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.request.SelectUniversityRequestDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response.SelectUniversityExamsResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response.SelectUniversityResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response.SelectUniversityUpdateResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.service.SelectUniversityService;
 import com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse;
 import com.nonsoolmate.nonsoolmateServer.global.security.AuthUser;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +42,14 @@ public class SelectUniversityController {
             @AuthUser Member member) {
         return ResponseEntity.ok().body(ApiResponse.success(GET_SELECT_UNIVERSITY_EXAMS_SUCCESS,
                 selectUniversityService.getSelectUniversityExams(member)));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApiResponse<SelectUniversityUpdateResponseDTO>> patchSelectUniversities(
+            @AuthUser Member member,
+            @RequestBody @Valid final List<SelectUniversityRequestDTO> request) {
+
+        return ResponseEntity.ok().body(ApiResponse.success(PATCH_SELECT_UNIVERSITIES_SUCCESS,
+                selectUniversityService.patchSelectUniversities(member, request)));
     }
 }
