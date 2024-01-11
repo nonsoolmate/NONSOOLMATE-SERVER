5d4540500b0bc2ec9ec9d24f6a8b10703a9ffd82
mikekks <thdalsrb79@naver.com>
2024-01-11 20:20:00 +0900

    [FEAT] 첨삭 PDF 저장 서비스 로직 개발

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/service/UniversityExamRecordService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/service/UniversityExamRecordService.java
@@ -9,6 +9,7 @@ import com.nonsoolmate.nonsoolmateServer.domain.university.entity.UniversityExam
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamException;
 import com.nonsoolmate.nonsoolmateServer.domain.university.repository.UniversityExamRepository;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.UniversityExamRecordResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.UniversityExamRecordResultResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.entity.UniversityExamRecord;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.repository.UniversityExamRecordRepository;
 import com.nonsoolmate.nonsoolmateServer.external.aws.service.CloudFrontService;
@@ -38,6 +39,20 @@ public class UniversityExamRecordService {
                 universityExamRecord.getExamRecordResultFileName());
 
         return UniversityExamRecordResponseDTO.of(answerUrl, resultUrl);
+    }
+
+    public UniversityExamRecordResultResponseDTO getUniversityExamRecordResult(Long universityExamId, Member member) {
+
+        UniversityExam universityExam = universityExamRepository.findByUniversityExamId(universityExamId)
+                .orElseThrow(() -> new UniversityExamException(NOT_FOUND_UNIVERSITY_EXAM));
+
+        UniversityExamRecord universityExamRecord = universityExamRecordRepository.findByUniversityExamAndMemberOrElseThrowException(
+                universityExam, member);
+
+        String resultUrl = cloudFrontService.createPreSignedGetUrl(EXAM_RESULT_FOLDER_NAME,
+                universityExamRecord.getExamRecordResultFileName());
+
+        return UniversityExamRecordResultResponseDTO.of(resultUrl);
     }
 
 }
