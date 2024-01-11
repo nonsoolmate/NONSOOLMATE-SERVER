4946a04708dfc6eca6a8676dd087941fbca7e6ad
mikekks <thdalsrb79@naver.com>
2024-01-11 22:58:21 +0900

    [FEAT] 목표 대학 "ㄱ-ㅎ"순 정렬 쿼리 작성

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/repository/SelectUniversityRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/repository/SelectUniversityRepository.java
@@ -6,11 +6,15 @@ import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.entity.SelectUn
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.University;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface SelectUniversityRepository extends JpaRepository<SelectUniversity, Long> {
 
     List<SelectUniversity> findAllByMember(Member member);
+
+    @Query("select su from SelectUniversity su where su.member =:member order by su.university.universityName asc, su.university.universityCollege asc")
+    List<SelectUniversity> findAllByMemberOrderByUniversityNameASCUniversityCollegeAsc(Member member);
 
     @Transactional
     void deleteByMemberAndUniversity(Member member, University university);
