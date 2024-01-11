62d27094f6eb76a4ccad618ebff8bc8bfe1264d4
mikekks <thdalsrb79@naver.com>
2024-01-11 20:19:25 +0900

    [FEAT] 첨삭 PDF 저장 controller단 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/UniversityExamRecordController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/UniversityExamRecordController.java
@@ -1,9 +1,11 @@
 package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller;
 
+import static com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.exception.UniversityExamRecordSuccessType.GET_UNIVERSITY_EXAM_RECORD_RESULT_SUCCESS;
 import static com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.exception.UniversityExamRecordSuccessType.GET_UNIVERSITY_EXAM_RECORD_SUCCESS;
 
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.UniversityExamRecordResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.UniversityExamRecordResultResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.service.UniversityExamRecordService;
 import com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse;
 import com.nonsoolmate.nonsoolmateServer.global.security.AuthUser;
@@ -28,5 +30,14 @@ public class UniversityExamRecordController {
     ) {
         return ResponseEntity.ok().body(ApiResponse.success(GET_UNIVERSITY_EXAM_RECORD_SUCCESS,
                 universityExamRecordService.getUniversityExamRecord(universityExamId, member)));
+    }
+
+    @GetMapping("/result/{id}")
+    public ResponseEntity<ApiResponse<UniversityExamRecordResultResponseDTO>> getUniversityExamRecordResult(
+            @PathVariable("id") Long universityExamId,
+            @AuthUser Member member
+    ){
+        return ResponseEntity.ok().body(ApiResponse.success(GET_UNIVERSITY_EXAM_RECORD_RESULT_SUCCESS,
+                universityExamRecordService.getUniversityExamRecordResult(universityExamId, member)));
     }
 }
