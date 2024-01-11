d0cf5c8f92ca84dbbb6d8c957dfef86aa9be0185
mikekks <thdalsrb79@naver.com>
2024-01-11 20:19:41 +0900

    [ADD] 첨삭 PDF 저장 DTO 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/UniversityExamRecordResultResponseDTO.java
@@ -0,0 +1,9 @@
+package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto;
+
+public record UniversityExamRecordResultResponseDTO(
+        String examResultUrl
+) {
+    public static UniversityExamRecordResultResponseDTO of(String examResultUrl){
+        return new UniversityExamRecordResultResponseDTO(examResultUrl);
+    }
+}
