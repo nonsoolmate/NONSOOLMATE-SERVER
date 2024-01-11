bf300bbce57c08a01822976ea06e48ca2294ce6e
sung-silver <jinna0319@gmail.com>
2024-01-11 20:59:58 +0900

    [STYLE] DTO response 폴더 내로 이동

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/response/UniversityExamInfoResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/response/UniversityExamInfoResponseDTO.java
@@ -1,4 +1,4 @@
-package com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto;
+package com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.response;
 
 public record UniversityExamInfoResponseDTO(Long examId, String examName, int examTimeLimit) {
     public static UniversityExamInfoResponseDTO of(Long examId, String examName, int examTimeLimit) {
