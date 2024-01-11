70769222a8644eb0016be338b5ed05384be2ac9f
sung-silver <jinna0319@gmail.com>
2024-01-12 01:48:39 +0900

    [ADD] 시험 기록 에러 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/exception/UniversityExamRecordExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/exception/UniversityExamRecordExceptionType.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum UniversityExamRecordExceptionType implements BusinessExceptionType {
-    NOT_FOUND_UNIVERSITY_EXAM_RECORD(HttpStatus.NOT_FOUND, "존재하지 않는 시험 응시 기록입니다.");
+    NOT_FOUND_UNIVERSITY_EXAM_RECORD(HttpStatus.NOT_FOUND, "존재하지 않는 시험 응시 기록입니다."),
+    CREATE_UNIVERSITY_EXAM_RECORD_FAIL(HttpStatus.BAD_REQUEST, "대학 시험 기록 생성에 실패했습니다.");
 
     private final HttpStatus status;
     private final String message;
