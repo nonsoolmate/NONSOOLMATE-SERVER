491ca083ba3bd19b602d91077c7e18bd62385434
sung-silver <jinna0319@gmail.com>
2024-01-12 21:41:08 +0900

    [ADD] Valid 관련 에러 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/error/exception/CommonErrorType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/error/exception/CommonErrorType.java
@@ -18,6 +18,8 @@ public enum CommonErrorType implements BusinessExceptionType {
      */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다"),
 
+    // 400 Bad Request
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다"),
     ;
 
     private final HttpStatus status;
