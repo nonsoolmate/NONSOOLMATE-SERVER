94d9ce978fe85e408dfc98576b6479393c7655e2
sung-silver <jinna0319@gmail.com>
2024-01-12 21:43:10 +0900

    [FEAT] SelectUniversityRequestDTO에 valid 요소 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/request/SelectUniversityRequestDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/request/SelectUniversityRequestDTO.java
@@ -1,6 +1,8 @@
 package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record SelectUniversityRequestDTO(
-        Long universityId
+        @NotNull Long universityId
 ) {
 }
