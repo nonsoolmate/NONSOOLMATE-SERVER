ce66b4eae4bf6d59b69e83ea86f742fe04e8e497
sung-silver <jinna0319@gmail.com>
2024-01-12 20:32:49 +0900

    [ADD] ExamImage folder 경로 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/FolderName.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/FolderName.java
@@ -4,4 +4,5 @@ public abstract class FolderName {
     public static final String EXAM_ANSWER_FOLDER_NAME = "exam-answer/";
     public static final String EXAM_RESULT_FOLDER_NAME = "exam-result/";
     public static final String EXAM_SHEET_FOLDER_NAME = "exam-sheet/";
+    public static final String EXAM_IMAGE_FOLDER_NAME = "exam-image/";
 }
