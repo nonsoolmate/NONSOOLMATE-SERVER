d562edcec19f58c724af19316d462b1f693a4d74
mikekks <thdalsrb79@naver.com>
2024-01-12 22:14:11 +0900

    [FIX] filter단에서의 예외처리를 위한 코드 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/security/filter/JwtAuthenticationFilter.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/security/filter/JwtAuthenticationFilter.java
@@ -1,13 +1,15 @@
 package com.nonsoolmate.nonsoolmateServer.global.security.filter;
 
 
-
+import static com.nonsoolmate.nonsoolmateServer.domain.auth.exception.AuthExceptionType.INVALID_ACCESS_TOKEN;
 import static com.nonsoolmate.nonsoolmateServer.domain.auth.exception.AuthExceptionType.UNAUTHORIZED_ACCESS_TOKEN;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nonsoolmate.nonsoolmateServer.domain.auth.exception.AuthException;
 import com.nonsoolmate.nonsoolmateServer.global.jwt.service.JwtService;
 import com.nonsoolmate.nonsoolmateServer.global.security.service.MemberAuthService;
 import com.nonsoolmate.nonsoolmateServer.global.jwt.utils.RequestUtils;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,25 +33,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final MemberAuthService memberAuthService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (RequestUtils.isContainsAccessToken(request)) {
-            String authorizationAccessToken = RequestUtils.getAuthorizationAccessToken((HttpServletRequest) request);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-            if (jwtService.validateToken(authorizationAccessToken)) {
-                Long memberId = jwtService.extractMemberIdFromAccessToken(authorizationAccessToken);
+        if (!RequestUtils.isContainsAccessToken(request)) {
+            return;
+        }
 
-                UserDetails userDetails = memberAuthService.loadUserByUsername(String.valueOf(memberId));
+        String authorizationAccessToken = RequestUtils.getAuthorizationAccessToken((HttpServletRequest) request);
 
-                Authentication authentication
-                        = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                log.info("Authentication Principal : {}", authentication.getPrincipal().toString());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            }
-            else {
+        try {
+            if (!jwtService.validateToken(authorizationAccessToken)) {
                 throw new AuthException(UNAUTHORIZED_ACCESS_TOKEN);
             }
+
+            Long memberId = jwtService.extractMemberIdFromAccessToken(authorizationAccessToken);
+
+            UserDetails userDetails = memberAuthService.loadUserByUsername(String.valueOf(memberId));
+
+            Authentication authentication
+                    = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            log.info("Authentication Principal : {}", authentication.getPrincipal().toString());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        } catch (JsonProcessingException e) {
+            throw new AuthException(INVALID_ACCESS_TOKEN);
+        } catch (AuthException e) {
+            throw new AuthException(e.getExceptionType());
         }
+
         filterChain.doFilter(request, response);
     }
 
