cfaaddf6e3f4c80100daba91ed08c1fe8c69d06a
sung-silver <jinna0319@gmail.com>
2024-01-12 21:43:38 +0900

    [FEAT] CreateUniversityExamRequestDTO valid 요소 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/request/CreateUniversityExamRequestDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/request/CreateUniversityExamRequestDTO.java
@@ -1,9 +1,11 @@
 package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record CreateUniversityExamRequestDTO(@NotNull Long universityExamId, @NotNull int memberTakeTimeExam,
-                                             String memberSheetFileName) {
+public record CreateUniversityExamRequestDTO(@NotNull Long universityExamId,
+                                             @Positive int memberTakeTimeExam,
+                                             @NotNull String memberSheetFileName) {
     public static CreateUniversityExamRequestDTO of(Long universityExamId, int memberTakeTimeExam,
                                                     String memberSheetFileName) {
         return new CreateUniversityExamRequestDTO(universityExamId, memberTakeTimeExam, memberSheetFileName);
