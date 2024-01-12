eec8fb07daec8db03bde4fa25ac537b83c635cc1
sung-silver <jinna0319@gmail.com>
2024-01-12 14:28:06 +0900

    [REFACTOR] FolderName 추상 클래스로 만들어 객체 생성 방지하기

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/FolderName.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/FolderName.java
@@ -1,9 +1,6 @@
 package com.nonsoolmate.nonsoolmateServer.external.aws;
 
-import lombok.Getter;
-
-@Getter
-public class FolderName {
+public abstract class FolderName {
     public static final String EXAM_ANSWER_FOLDER_NAME = "exam-answer/";
     public static final String EXAM_RESULT_FOLDER_NAME = "exam-result/";
     public static final String EXAM_SHEET_FOLDER_NAME = "exam-sheet/";
