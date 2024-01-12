569a64af838a98485d01fcfef89d60d7cf41c768
mikekks <thdalsrb79@naver.com>
2024-01-12 22:14:29 +0900

    [FEAT] JwtExceptionFilter 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/security/filter/JwtExceptionFilter.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/security/filter/JwtExceptionFilter.java
@@ -1,4 +1,35 @@
 package com.nonsoolmate.nonsoolmateServer.global.security.filter;
 
-public class JwtExceptionFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nonsoolmate.nonsoolmateServer.domain.auth.exception.AuthException;
+import com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (AuthException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            String error = objectMapper.writeValueAsString(ApiResponse.error(e.getExceptionType()));
+            response.getWriter().write(error);
+        }
+    }
 }
