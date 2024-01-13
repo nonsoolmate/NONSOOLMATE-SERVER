97ef9cca4e5de1c299fc7ad9f9043a29416e7ac5
mikekks <thdalsrb79@naver.com>
2024-01-13 22:10:42 +0900

    [FIX] 코드리뷰 반영

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/repository/SelectUniversityRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/repository/SelectUniversityRepository.java
@@ -5,6 +5,7 @@ import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.entity.SelectUniversity;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.University;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,8 @@ public interface SelectUniversityRepository extends JpaRepository<SelectUniversi
 
     @Query("select su from SelectUniversity su where su.member =:member order by su.university.universityName asc, su.university.universityCollege asc")
     List<SelectUniversity> findAllByMemberOrderByUniversityNameASCUniversityCollegeAsc(Member member);
+
+    Optional<SelectUniversity> findByMemberAndUniversity(Member member, University university);
 
     @Transactional
     void deleteByMemberAndUniversity(Member member, University university);
