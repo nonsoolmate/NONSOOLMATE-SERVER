595ec686bf58024d159da80b9f0040e0e8be17f0
mikekks <thdalsrb79@naver.com>
2024-01-14 22:24:09 +0900

    [ADD] DTO에 universityExamName 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/response/UniversityExamRecordResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/response/UniversityExamRecordResponseDTO.java
@@ -1,10 +1,11 @@
 package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.response;
 
 public record UniversityExamRecordResponseDTO(
+        String universityExamName,
         String examAnswerUrl,
         String examResultUrl
 ) {
-    public static UniversityExamRecordResponseDTO of(String examAnswerUrl, String examResultUrl) {
-        return new UniversityExamRecordResponseDTO(examAnswerUrl, examResultUrl);
+    public static UniversityExamRecordResponseDTO of(String universityExamName, String examAnswerUrl, String examResultUrl) {
+        return new UniversityExamRecordResponseDTO(universityExamName, examAnswerUrl, examResultUrl);
     }
 }
