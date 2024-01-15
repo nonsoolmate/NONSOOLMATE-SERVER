c3fca71bb098fabbc2b586d120883ad1e3367edb
mikekks <thdalsrb79@naver.com>
2024-01-15 21:24:21 +0900

    [REFACTOR] AuthController 스웨거 인터페이스 생성

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/auth/controller/AuthApi.java
@@ -0,0 +1,37 @@
+package com.nonsoolmate.nonsoolmateServer.domain.auth.controller;
+
+import com.nonsoolmate.nonsoolmateServer.domain.auth.controller.dto.request.MemberRequestDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.auth.controller.dto.response.MemberAuthResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.auth.controller.dto.response.MemberReissueResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+
+
+@Tag(name = "auth", description = "인증 관련 API")
+public interface AuthApi {
+
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "404", content = @Content)
+            }
+    )
+
+    @Operation(summary = "소셜 로그인", description = "네이버 소셜 로그인을 합니다.")
+    ResponseEntity<com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse<MemberAuthResponseDTO>> login(
+            @Parameter(description = "인가 코드", required = true) final String authorizationCode,
+            @Parameter(description = "플랫폼 타입(ex.'NAVER')", required = true) final MemberRequestDTO request,
+            HttpServletResponse response);
+
+    @Operation(summary = "액세스 토큰 & 리프레시 토큰 재발급", description = "액세스 토큰 및 리프레시 토큰을 재발급 받습니다.")
+    ResponseEntity<com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse<MemberReissueResponseDTO>> reissue(
+            HttpServletRequest request,
+            HttpServletResponse response);
+}
