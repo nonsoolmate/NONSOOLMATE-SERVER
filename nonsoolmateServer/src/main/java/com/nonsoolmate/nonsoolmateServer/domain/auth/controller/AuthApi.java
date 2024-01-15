b8c7b3b4ed076f796214083c7d30a8201e4d4e22
sung-silver <jinna0319@gmail.com>
2024-01-15 23:12:40 +0900

    [FIX] auth swagger 적용에서 오버라이드 및 메서드 인자 앞 어노테이션 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/auth/controller/AuthApi.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/auth/controller/AuthApi.java
@@ -11,7 +11,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 
 @Tag(name = "auth", description = "인증 관련 API")
@@ -26,8 +29,8 @@ public interface AuthApi {
 
     @Operation(summary = "소셜 로그인", description = "네이버 소셜 로그인을 합니다.")
     ResponseEntity<com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse<MemberAuthResponseDTO>> login(
-            @Parameter(description = "인가 코드", required = true) final String authorizationCode,
-            @Parameter(description = "플랫폼 타입(ex.'NAVER')", required = true) final MemberRequestDTO request,
+            @Parameter(description = "인가 코드", required = true) @RequestHeader(value = "authorization-code") final String authorizationCode,
+            @Parameter(description = "플랫폼 타입(ex.'NAVER')", required = true) @RequestBody @Valid final MemberRequestDTO request,
             HttpServletResponse response);
 
     @Operation(summary = "액세스 토큰 & 리프레시 토큰 재발급", description = "액세스 토큰 및 리프레시 토큰을 재발급 받습니다.")
