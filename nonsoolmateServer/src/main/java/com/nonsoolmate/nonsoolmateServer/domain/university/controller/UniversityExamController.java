7944699b2b3eeb6d9d4c4aec70f27a60a7661292
mikekks <thdalsrb79@naver.com>
2024-01-15 14:53:43 +0900

    [FEAT] UniversityExamController 스웨거 적용

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/UniversityExamController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/UniversityExamController.java
@@ -8,6 +8,8 @@ import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.respon
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.UniversityExamSuccessType;
 import com.nonsoolmate.nonsoolmateServer.domain.university.service.UniversityExamService;
 import com.nonsoolmate.nonsoolmateServer.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,9 +24,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/university/exam")
 @RequiredArgsConstructor
+@Tag(name = "UniversityExam", description = "대학 시험 정보와 관련된 API")
 public class UniversityExamController {
     private final UniversityExamService universityExamService;
 
+    @Operation(summary = "시험 보기: 시험 이름 & 제한 시간", description = "시험 응시 화면의 이름 및 제한 시간을 조회합니다.")
     @GetMapping("/{id}/info")
     public ResponseEntity<ApiResponse<UniversityExamInfoResponseDTO>> getUniversityExam(
             @PathVariable("id") Long universityExamId) {
@@ -32,6 +36,7 @@ public class UniversityExamController {
                 universityExamService.getUniversityExam(universityExamId)));
     }
 
+    @Operation(summary = "시험 보기: 문제지 [페이지네이션]", description = "시험 응시 화면의 문제지를 조회합니다.")
     @GetMapping("{id}/image")
     public ResponseEntity<ApiResponse<Page<UniversityExamImageResponseDTO>>> getUniversityExamImages(
             @PathVariable("id") Long id, @RequestParam(defaultValue = "0") int page, Pageable pageable) {
@@ -41,7 +46,7 @@ public class UniversityExamController {
                 .body(ApiResponse.success(UniversityExamSuccessType.GET_UNIVERSITY_EXAM_IMAGE_SUCCESS, images));
     }
 
-
+    @Operation(summary = "해제: 문제이미지_해제PDF", description = "문제 이미지 및 해제 pdf를 조회합니다.")
     @GetMapping("{id}/answer")
     public ResponseEntity<ApiResponse<UniversityExamImageAndAnswerResponseDTO>> getUniversityExamImageAndAnswer(
             @PathVariable("id") Long universityExamId
