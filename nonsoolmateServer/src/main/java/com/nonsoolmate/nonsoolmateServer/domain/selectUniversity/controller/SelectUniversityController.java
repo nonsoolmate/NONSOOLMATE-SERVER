5a168fe8958f257a7166e2a28148e493fc363de5
mikekks <thdalsrb79@naver.com>
2024-01-15 21:26:24 +0900

    [DEL] SelectUniversityController 필요없는 코드 삭제

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/SelectUniversityController.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/SelectUniversityController.java
@@ -27,12 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/select-university")
-@Tag(name = "SelectUniversity", description = "목표 대학과 관련된 API")
-public class SelectUniversityController {
+public class SelectUniversityController implements SelectUniversityApi {
 
     private final SelectUniversityService selectUniversityService;
 
-    @Operation(summary = "목표대학 설정: 리스트 조회", description = "내 목표 대학 리스트를 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<List<SelectUniversityResponseDTO>>> getSelectUniversities(
             @AuthUser Member member) {
@@ -41,7 +39,6 @@ public class SelectUniversityController {
                 selectUniversityService.getSelectUniversities(member)));
     }
 
-    @Operation(summary = "마이 페이지: 대학별 시험 리스트 조회", description = "내 목표 대학들의 시험 리스트를 조회합니다.")
     @GetMapping("/exam")
     public ResponseEntity<ApiResponse<List<SelectUniversityExamsResponseDTO>>> getSelectUniversityExams(
             @AuthUser Member member) {
@@ -49,7 +46,6 @@ public class SelectUniversityController {
                 selectUniversityService.getSelectUniversityExams(member)));
     }
 
-    @Operation(summary = "목표대학 설정: 리스트 선택", description = "내 목표 대학들 리스트를 업데이트(수정) 합니다.")
     @PatchMapping
     public ResponseEntity<ApiResponse<SelectUniversityUpdateResponseDTO>> patchSelectUniversities(
             @AuthUser Member member,
