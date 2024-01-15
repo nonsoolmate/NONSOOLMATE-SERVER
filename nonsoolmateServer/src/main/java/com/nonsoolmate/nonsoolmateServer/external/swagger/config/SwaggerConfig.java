a23380f6498cd9c004eb55edd9285b0fcdbd5046
mikekks <thdalsrb79@naver.com>
2024-01-15 15:23:46 +0900

    [FEAT] 스웨거 security schema 적용

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/swagger/config/SwaggerConfig.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/swagger/config/SwaggerConfig.java
@@ -1,13 +1,35 @@
 package com.nonsoolmate.nonsoolmateServer.external.swagger.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "NONSOOLMATE",
                 description = "NONSOOLMATE SERVER API Documentation",
-                version = "v1"))
+                version = "v1"),
+        security = {
+                @SecurityRequirement(name = "Authorization"),
+                @SecurityRequirement(name = "Authorization-refresh")
+        })
+@SecuritySchemes({
+        @SecurityScheme(name = "Authorization",
+                type = SecuritySchemeType.HTTP,
+                description = "access token",
+                in = SecuritySchemeIn.HEADER,
+                paramName = "Authorization"),
+        @SecurityScheme(name = "Authorization-refresh",
+                type = SecuritySchemeType.APIKEY,
+                description = "refresh token",
+                in = SecuritySchemeIn.HEADER,
+                paramName = "Authorization-refresh"),
+})
 @Configuration
 public class SwaggerConfig {
+
 }
