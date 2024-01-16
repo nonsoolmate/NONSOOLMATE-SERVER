909d9dcfc6bee21408566e025d188f0a6ce22511
mikekks <thdalsrb79@naver.com>
2024-01-16 19:01:45 +0900

    [FIX] 에러 메세지 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/exception/UniversityExamRecordExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/exception/UniversityExamRecordExceptionType.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum UniversityExamRecordExceptionType implements BusinessExceptionType {
     NOT_FOUND_UNIVERSITY_EXAM_RECORD(HttpStatus.NOT_FOUND, "존재하지 않는 시험 응시 기록입니다."),
     CREATE_UNIVERSITY_EXAM_RECORD_FAIL(HttpStatus.BAD_REQUEST, "대학 시험 기록 생성에 실패했습니다."),
+    INVALID_UNIVERSITY_EXAM_RECORD_RESULT_FILE_NAME(HttpStatus.BAD_REQUEST, "첨삭이 완료되지 않았습니다."),
+
     ALREADY_CREATE_EXAM_RECORD(HttpStatus.BAD_REQUEST, "이미 응시한 대학 시험입니다");
 
     private final HttpStatus status;
