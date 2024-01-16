134be633257280200c76e8faf5a36910b518338e
mikekks <thdalsrb79@naver.com>
2024-01-16 18:00:39 +0900

    [FIX] 서버 오류 메세지 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/error/exception/CommonErrorType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/error/exception/CommonErrorType.java
@@ -16,7 +16,7 @@ public enum CommonErrorType implements BusinessExceptionType {
     /**
      * 500 Internal Server Error
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
 
     // 400 Bad Request
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다"),
