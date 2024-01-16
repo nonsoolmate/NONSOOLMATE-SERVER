428f18b8a06761e9d0162a17c230c9b3d7629fa7
mikekks <thdalsrb79@naver.com>
2024-01-16 17:35:09 +0900

    [ADD] 입력 오류 처리를 위한 String으로 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/auth/controller/dto/request/MemberRequestDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/auth/controller/dto/request/MemberRequestDTO.java
@@ -1,7 +1,11 @@
 package com.nonsoolmate.nonsoolmateServer.domain.auth.controller.dto.request;
 
-import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.PlatformType;
+
 import jakarta.validation.constraints.NotNull;
 
-public record MemberRequestDTO(@NotNull PlatformType platformType) {
+
+public record MemberRequestDTO(
+        @NotNull
+        String platformType) {
+
 }
