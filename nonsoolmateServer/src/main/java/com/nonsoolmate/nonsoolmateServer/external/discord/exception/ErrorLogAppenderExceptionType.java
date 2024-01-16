5431ccae3a00011cfb70c9dac275d409899ab405
mikekks <thdalsrb79@naver.com>
2024-01-17 02:26:45 +0900

    [FEAT] Appender 예외 처리 enum 추가

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/discord/exception/ErrorLogAppenderExceptionType.java
@@ -0,0 +1,26 @@
+package com.nonsoolmate.nonsoolmateServer.external.discord.exception;
+
+import com.nonsoolmate.nonsoolmateServer.global.error.exception.BusinessExceptionType;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorLogAppenderExceptionType implements BusinessExceptionType {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다"),
+    DISCORD_LOG_APPENDER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "디스코드 로그 전송에 실패하였습니다"),
+    API_CALL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "외부 API CALL에 실패하였습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus status() {
+        return this.status;
+    }
+
+    @Override
+    public String message() {
+        return this.message;
+    }
+}
