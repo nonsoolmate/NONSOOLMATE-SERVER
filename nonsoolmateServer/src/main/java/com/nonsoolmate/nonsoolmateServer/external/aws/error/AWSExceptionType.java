f34f066d70c874cba767aced612d76e81ae41da4
mikekks <thdalsrb79@naver.com>
2024-01-16 19:01:39 +0900

    [FIX] 에러 메세지 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/error/AWSExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/error/AWSExceptionType.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AWSExceptionType implements BusinessExceptionType {
-    DELETE_FILE_AWS_S3_FAIL(HttpStatus.BAD_REQUEST, "올바른 답안지 파일 이름이 아닙니다."),
-    NOT_FOUND_SHEET_FILE_AWS_S3(HttpStatus.NOT_FOUND, "올바른 답안지 파일 이름을 찾을 수 없습니다"),
+    DELETE_FILE_AWS_S3_FAIL(HttpStatus.BAD_REQUEST, "s3에 해당 파일 삭제에 실패했습니다."),
+    NOT_FOUND_SHEET_FILE_AWS_S3(HttpStatus.NOT_FOUND, "해당 답안지 파일 이름을 찾을 수 없습니다"),
     GET_PRESIGNED_URL_AWS_CLOUDFRONT_FAIL(HttpStatus.BAD_REQUEST, "Cloud PresignedUrl 발급에 실패했습니다");
 
     private final HttpStatus status;
