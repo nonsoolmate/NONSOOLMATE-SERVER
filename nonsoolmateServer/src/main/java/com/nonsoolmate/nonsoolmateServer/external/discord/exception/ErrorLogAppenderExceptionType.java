e00fcd2c9d8ea7021924ad12972c0a8a5b98298f
mikekks <thdalsrb79@naver.com>
2024-01-17 03:25:19 +0900

    [CHORE] 예외 처리 변경으로 인한 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/discord/exception/ErrorLogAppenderExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/discord/exception/ErrorLogAppenderExceptionType.java
@@ -1,12 +1,12 @@
 package com.nonsoolmate.nonsoolmateServer.external.discord.exception;
 
-import com.nonsoolmate.nonsoolmateServer.global.error.exception.BusinessExceptionType;
+import com.nonsoolmate.nonsoolmateServer.global.error.exception.ExceptionType;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public enum ErrorLogAppenderExceptionType implements BusinessExceptionType {
+public enum ErrorLogAppenderExceptionType implements ExceptionType {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다"),
     DISCORD_LOG_APPENDER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "디스코드 로그 전송에 실패하였습니다"),
     API_CALL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "외부 API CALL에 실패하였습니다.");
