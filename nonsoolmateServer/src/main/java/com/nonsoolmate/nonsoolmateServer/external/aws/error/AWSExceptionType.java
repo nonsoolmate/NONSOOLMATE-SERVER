585f796c6b73ad72ae7ef5083b246b8b0eacf15c
sung-silver <jinna0319@gmail.com>
2024-01-17 00:49:14 +0900

    [FIX] AWS에서 ClientException, BusinessException 분리

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/error/AWSExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/error/AWSExceptionType.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AWSExceptionType implements ExceptionType {
     DELETE_FILE_AWS_S3_FAIL(HttpStatus.BAD_REQUEST, "s3에 해당 파일 삭제에 실패했습니다."),
+    NOT_FOUND_AWS_PRIVATE_KEY(HttpStatus.NOT_FOUND, "해당 AWS Private Key를 찾을 수 없습니다"),
     NOT_FOUND_SHEET_FILE_AWS_S3(HttpStatus.NOT_FOUND, "해당 답안지 파일 이름을 찾을 수 없습니다"),
-    GET_PRESIGNED_URL_AWS_CLOUDFRONT_FAIL(HttpStatus.BAD_REQUEST, "Cloud PresignedUrl 발급에 실패했습니다");
+    GET_PRESIGNED_URL_AWS_CLOUDFRONT_FAIL(HttpStatus.BAD_REQUEST, "Cloud PreSignedUrl 발급에 실패했습니다"),
+    AWS_S3_SERVICE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "S3 서비스 에러");
 
     private final HttpStatus status;
     private final String message;
