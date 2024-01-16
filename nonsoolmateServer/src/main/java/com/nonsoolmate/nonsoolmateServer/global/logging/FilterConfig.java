a2cf846520991aa3fea43d2717ba45d55d6292f8
mikekks <thdalsrb79@naver.com>
2024-01-17 02:26:14 +0900

    [FEAT] 로깅 관련된 필터 config에 설정

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/global/logging/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.nonsoolmate.nonsoolmateServer.global.logging;
+
+import com.nonsoolmate.nonsoolmateServer.global.logging.filter.MDCFilter;
+import com.nonsoolmate.nonsoolmateServer.global.logging.filter.ServletWrappingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!local")
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<ServletWrappingFilter> secondFilter() {
+        FilterRegistrationBean<ServletWrappingFilter> filterRegistrationBean = new FilterRegistrationBean<>(
+                new ServletWrappingFilter());
+        filterRegistrationBean.setOrder(0);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<MDCFilter> thirdFilter() {
+        FilterRegistrationBean<MDCFilter> filterRegistrationBean = new FilterRegistrationBean<>(
+                new MDCFilter());
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+}
