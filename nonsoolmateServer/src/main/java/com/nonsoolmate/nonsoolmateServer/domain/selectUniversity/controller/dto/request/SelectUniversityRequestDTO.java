809054c290b8c0f8f1aebfd5b0270089c59e710a
sung-silver <jinna0319@gmail.com>
2024-01-17 21:20:43 +0900

    [FEAT] selectUniversity 패키지에 Swagger (에러, DTO) 적용

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/request/SelectUniversityRequestDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/request/SelectUniversityRequestDTO.java
@@ -1,8 +1,9 @@
 package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record SelectUniversityRequestDTO(
-        @NotNull Long universityId
+        @NotNull @Schema(name="사용자가 선택한 대학 id", example="1") Long universityId
 ) {
 }
--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/response/SelectUniversityResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/response/SelectUniversityResponseDTO.java
@@ -1,10 +1,15 @@
 package com.nonsoolmate.nonsoolmateServer.domain.selectUniversity.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "SelectUniversityResponseDTO", description = "선택 대학 조회 응답 DTO")
 public record SelectUniversityResponseDTO(
-    Long universityId, String universityName, String collegeName, boolean memberStatus
+        @Schema(name = "대학 id", example = "1") Long universityId,
+        @Schema(name = "대학 이름", example = "중앙대학교") String universityName,
+        @Schema(name = "단과대학 이름", example = "경영경제") String collegeName,
+        @Schema(name = "대학 선택 여부", example = "true") boolean memberStatus
 ) {
-    static public SelectUniversityResponseDTO of(Long universityId, String universityName, String collegeName, boolean memberStatus) {
+    public static SelectUniversityResponseDTO of(Long universityId, String universityName, String collegeName, boolean memberStatus) {
         return new SelectUniversityResponseDTO(universityId, universityName, collegeName, memberStatus);
     }
 }
