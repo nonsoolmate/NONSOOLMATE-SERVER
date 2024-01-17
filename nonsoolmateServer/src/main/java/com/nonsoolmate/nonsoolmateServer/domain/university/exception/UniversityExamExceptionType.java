19c927e463625bcfd127f13cfc170e2d6b129f38
sung-silver <jinna0319@gmail.com>
2024-01-17 22:27:06 +0900

    [FIX] UniversityExamExceptionType에서 같은 에러 메시지 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/exception/UniversityExamExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/exception/UniversityExamExceptionType.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum UniversityExamExceptionType implements ExceptionType {
 
+    /**
+     * 404 Not Found
+     */
     INVALID_UNIVERSITY_EXAM(HttpStatus.BAD_REQUEST, "존재하지 않는 대학 시험입니다."),
-    NOT_FOUND_UNIVERSITY_EXAM_IMAGE(HttpStatus.NOT_FOUND, "존재하지 않는 대학 시험 입니다.");
+    NOT_FOUND_UNIVERSITY_EXAM_IMAGE(HttpStatus.NOT_FOUND, "존재하지 않는 대학 시험 이미지 입니다.");
 
     private final HttpStatus status;
     private final String message;
