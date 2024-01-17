d64565adaa686c67e72a0029ee1a059baf95f389
sung-silver <jinna0319@gmail.com>
2024-01-17 23:34:50 +0900

    [FEAT] universityExamRecord 패키지 아래 Swagger(에러, DTO) 적용

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/UniversityExamRecordApi.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/UniversityExamRecordApi.java
@@ -6,11 +6,14 @@ import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.response.UniversityExamRecordResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.response.UniversityExamRecordResultResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.response.UniversityExamSheetPreSignedUrlResponseDTO;
+import com.nonsoolmate.nonsoolmateServer.global.response.ErrorResponse;
 import com.nonsoolmate.nonsoolmateServer.global.response.SuccessResponse;
 import com.nonsoolmate.nonsoolmateServer.global.security.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -24,27 +27,46 @@ public interface UniversityExamRecordApi {
 
     @ApiResponses(
             value = {
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200"),
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", content = @Content),
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", content = @Content)
+                    @ApiResponse(responseCode = "200", description = "첨삭 PDF, 해제 PDF 조회에 성공했습니다."),
+                    @ApiResponse(responseCode = "400", description = "존재하지 않는 대학 시험입니다, 첨삭이 완료되지 않았습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 시험 응시 기록입니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
             }
     )
-
     @Operation(summary = "첨삭: 첨삭 PDF_해제PDF", description = "첨삭 pdf 및 해제 pdf를 조회합니다.")
     ResponseEntity<SuccessResponse<UniversityExamRecordResponseDTO>> getUniversityExamRecord(
             @Parameter(description = "해당 대학교 시험 Id (examId)", required = true) @PathVariable("id") Long universityExamId,
             @AuthUser Member member);
 
+
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "첨삭 PDF 조회에 성공했습니다."),
+                    @ApiResponse(responseCode = "400", description = "존재하지 않는 대학 시험입니다, 첨삭이 완료되지 않았습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 시험 응시 기록입니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @Operation(summary = "첨삭: 첨삭 PDF 저장", description = "첨삭 pdf를 조회합니다.")
     ResponseEntity<SuccessResponse<UniversityExamRecordResultResponseDTO>> getUniversityExamRecordResult(
             @Parameter(description = "해당 대학교 시험 Id (examId)", required = true) @PathVariable("id") Long universityExamId,
             @AuthUser Member member);
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "답안지 업로드 PresignedUrl 발급에 성공했습니다.")
+            }
+    )
     @Operation(summary = "시험 보기: [1] 답안지 업로드 PresignedUrl 조회 API", description = "답안지(시험응시기록) 업로드를 위한 PresignedUrl를 조회합니다.")
     ResponseEntity<SuccessResponse<UniversityExamSheetPreSignedUrlResponseDTO>> getUniversityExamSheetPreSignedUrl();
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "대학 시험 기록에 성공했습니다."),
+                    @ApiResponse(responseCode = "400", description = "존재하지 않는 대학 시험입니다, 이미 응시한 대학 시험입니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "해당 답안지 파일 이름을 찾을 수 없습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @Operation(summary = "시험보기: [3] 답안지 업로드 후 시험 기록 API", description = "답안지(시험응시기록) 업로드 후 서버에 기록하기 위해 호출합니다.")
     ResponseEntity<SuccessResponse<UniversityExamRecordIdResponse>> createUniversityExamRecord(
-            @Parameter(description = "해당 대학교 시험 Id (examId), 시험 응시 소요 시간, 답안지(응시 기록) 파일 이름", required = true) @Valid @RequestBody CreateUniversityExamRequestDTO createUniversityExamRequestDTO,
+            @Valid @RequestBody CreateUniversityExamRequestDTO createUniversityExamRequestDTO,
             @AuthUser Member member);
 }
--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/request/CreateUniversityExamRequestDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/controller/dto/request/CreateUniversityExamRequestDTO.java
@@ -1,11 +1,13 @@
 package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record CreateUniversityExamRequestDTO(@NotNull Long universityExamId,
-                                             @Positive int memberTakeTimeExam,
-                                             @NotNull String memberSheetFileName) {
+@Schema(name = "CreateUniversityExamRequestDTO", description = "대학 시험 기록 생성 요청 DTO")
+public record CreateUniversityExamRequestDTO(@NotNull @Schema(description = "대학 시험 id", example="1") Long universityExamId,
+                                             @Positive @Schema(description = "사용자가 해당 시험을 보는데 걸린 시간(초 단위)") int memberTakeTimeExam,
+                                             @NotNull @Schema(description="PreSingedUrl 발급 시 전달 받은 파일 이름") String memberSheetFileName) {
     public static CreateUniversityExamRequestDTO of(Long universityExamId, int memberTakeTimeExam,
                                                     String memberSheetFileName) {
         return new CreateUniversityExamRequestDTO(universityExamId, memberTakeTimeExam, memberSheetFileName);
