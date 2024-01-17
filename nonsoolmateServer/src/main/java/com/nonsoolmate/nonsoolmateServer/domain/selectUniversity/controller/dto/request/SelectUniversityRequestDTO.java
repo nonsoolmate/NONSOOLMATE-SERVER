693dbcd5bcf20bbcf87fc25095c59772d6187475
sung-silver <jinna0319@gmail.com>
2024-01-17 21:26:39 +0900

    [FIX] name 속성을 description으로 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/request/SelectUniversityRequestDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectUniversity/controller/dto/request/SelectUniversityRequestDTO.java
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record SelectUniversityRequestDTO(
-        @NotNull @Schema(name="사용자가 선택한 대학 id", example="1") Long universityId
+        @NotNull @Schema(description="사용자가 선택한 대학 id", example="1") Long universityId
 ) {
 }
