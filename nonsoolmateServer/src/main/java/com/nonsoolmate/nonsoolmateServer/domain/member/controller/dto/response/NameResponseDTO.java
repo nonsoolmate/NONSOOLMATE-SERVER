0461976174cd88392b4b5930b096e0baa9cc5023
sung-silver <jinna0319@gmail.com>
2024-01-17 18:59:39 +0900

    [FEAT] MemberApi Swagger 에러 응답 추가 및 dto에 Swagger 적용

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/NameResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/NameResponseDTO.java
@@ -1,6 +1,9 @@
 package com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response;
 
-public record NameResponseDTO(String memberName) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "NameResponseDTO", description = "멤버 이름 응답 DTO")
+public record NameResponseDTO(@Schema(description = "멤버 이름", example = "류가은") String memberName) {
     static public NameResponseDTO of(String memberName) {
         return new NameResponseDTO(memberName);
     }
--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/TicketResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/TicketResponseDTO.java
@@ -1,6 +1,10 @@
 package com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response;
 
-public record TicketResponseDTO(String memberName, int ticketCount) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "TicketResponseDTO", description = "멤버 첨삭권 응답 DTO")
+public record TicketResponseDTO(@Schema(description = "멤버 이름", example = "류가은") String memberName,
+                                @Schema(description = "사용자 첨삭권 개수", example = "5") int ticketCount) {
     static public TicketResponseDTO of(String memberName, int ticketCount) {
         return new TicketResponseDTO(memberName, ticketCount);
     }
