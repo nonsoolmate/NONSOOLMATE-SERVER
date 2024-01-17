ea8a5c9005c6c826f58beaadf350d90b3320bb4d
sung-silver <jinna0319@gmail.com>
2024-01-17 21:26:16 +0900

    [FIX] public static 컨벤션 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/NameResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/controller/dto/response/NameResponseDTO.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "NameResponseDTO", description = "멤버 이름 응답 DTO")
 public record NameResponseDTO(@Schema(description = "멤버 이름", example = "류가은") String memberName) {
-    static public NameResponseDTO of(String memberName) {
+    public static NameResponseDTO of(String memberName) {
         return new NameResponseDTO(memberName);
     }
 }
