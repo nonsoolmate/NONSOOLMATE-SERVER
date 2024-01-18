9404c8b34e1e5b4586f923eeab3ce4b24403758b
sung-silver <jinna0319@gmail.com>
2024-01-19 05:28:28 +0900

    [DEL] S3Service에서 PreSignedUrl 로직 삭제

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/S3Service.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/S3Service.java
@@ -2,9 +2,7 @@ package com.nonsoolmate.nonsoolmateServer.external.aws.service;
 
 import com.nonsoolmate.nonsoolmateServer.external.aws.config.AWSConfig;
 import com.nonsoolmate.nonsoolmateServer.external.aws.error.*;
-import com.nonsoolmate.nonsoolmateServer.external.aws.service.vo.PreSignedUrlVO;
-import java.time.Duration;
-import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,12 +10,7 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
-import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
-import java.net.URL;
 
 @Service
 @RequiredArgsConstructor
@@ -26,32 +19,6 @@ public class S3Service {
     @Value("${aws-property.s3-bucket-name}")
     private String bucketName;
     private final AWSConfig awsConfig;
-    private static final Long PRE_SIGNED_URL_EXPIRE_MINUTE = 1L;
-
-    public PreSignedUrlVO getUploadPreSignedUrl(final String prefix) {
-        String uuidFileName = generateZipFileName();
-        String key = prefix + uuidFileName;
-
-        S3Presigner preSigner = awsConfig.getS3Presigner();
-
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-
-        PutObjectPresignRequest preSignedUrlRequest = PutObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(PRE_SIGNED_URL_EXPIRE_MINUTE))
-                .putObjectRequest(putObjectRequest)
-                .build();
-
-        URL url = preSigner.presignPutObject(preSignedUrlRequest).url();
-
-        return PreSignedUrlVO.of(uuidFileName, url.toString());
-    }
-
-    private String generateZipFileName() {
-        return UUID.randomUUID() + ".zip";
-    }
 
     public String validateURL(final String prefix, final String fileName) {
         try {
