eff8b54c9388d7bf886cc7eaca7fb95476deafa9
sung-silver <jinna0319@gmail.com>
2024-01-19 05:31:00 +0900

    [FIX] UniversityExamRecordSheetService 내 getUniversityExamRecordSheetPreSignedUrl()에서 사용 서비스 및 메서드 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/service/UniversityExamRecordSheetService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/universityExamRecord/service/UniversityExamRecordSheetService.java
@@ -2,7 +2,7 @@ package com.nonsoolmate.nonsoolmateServer.domain.universityExamRecord.service;
 
 import static com.nonsoolmate.nonsoolmateServer.external.aws.FolderName.EXAM_SHEET_FOLDER_NAME;
 
-import com.nonsoolmate.nonsoolmateServer.external.aws.service.S3Service;
+import com.nonsoolmate.nonsoolmateServer.external.aws.service.CloudFrontService;
 import com.nonsoolmate.nonsoolmateServer.external.aws.service.vo.PreSignedUrlVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UniversityExamRecordSheetService {
-    private final S3Service s3Service;
+    private final CloudFrontService cloudFrontService;
 
     public PreSignedUrlVO getUniversityExamRecordSheetPreSignedUrl() {
-        return s3Service.getUploadPreSignedUrl(EXAM_SHEET_FOLDER_NAME);
+        return cloudFrontService.createPreSignedPutUrl(EXAM_SHEET_FOLDER_NAME);
     }
 
 }
