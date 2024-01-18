819e99e86248635ae7529d0d9c46407fb6fcaf70
sung-silver <jinna0319@gmail.com>
2024-01-19 05:29:15 +0900

    [ADD] 파일 이름 인코딩 로직 에러 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/error/AWSExceptionType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/error/AWSExceptionType.java
@@ -11,6 +11,8 @@ public enum AWSExceptionType implements ExceptionType {
     NOT_FOUND_AWS_PRIVATE_KEY(HttpStatus.NOT_FOUND, "해당 AWS Private Key를 찾을 수 없습니다"),
     NOT_FOUND_SHEET_FILE_AWS_S3(HttpStatus.NOT_FOUND, "해당 답안지 파일 이름을 찾을 수 없습니다"),
     GET_PRESIGNED_URL_AWS_CLOUDFRONT_FAIL(HttpStatus.BAD_REQUEST, "Cloud PreSignedUrl 발급에 실패했습니다"),
+
+    FAIL_ENCODING_FILE_NAME(HttpStatus.BAD_REQUEST, "파일 이름 인코딩에 실패했습니다"),
     AWS_S3_SERVICE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "S3 서비스 에러");
 
     private final HttpStatus status;
