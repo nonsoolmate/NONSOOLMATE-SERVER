2813000938386e16675380177e41c7645dd131a0
sung-silver <jinna0319@gmail.com>
2024-01-19 05:29:54 +0900

    [FEAT] CloudFront PreSignedUrl에서 upload 로직 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/CloudFrontService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/service/CloudFrontService.java
@@ -2,12 +2,16 @@ package com.nonsoolmate.nonsoolmateServer.external.aws.service;
 
 import com.nonsoolmate.nonsoolmateServer.external.aws.error.AWSBusinessException;
 import com.nonsoolmate.nonsoolmateServer.external.aws.error.AWSExceptionType;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+import com.nonsoolmate.nonsoolmateServer.external.aws.service.vo.PreSignedUrlVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,9 +36,22 @@ public class CloudFrontService {
 
     private static final Long PRE_SIGNED_URL_EXPIRE_SECONDS = 30L;
 
+    public PreSignedUrlVO createPreSignedPutUrl(String path) {
+        String fileName = generateZipFileName();
+        return PreSignedUrlVO.of(fileName, createPreSignedUrl(path + fileName));
+    }
+
+    private String generateZipFileName() {
+        return UUID.randomUUID() + ".zip";
+    }
+
     public String createPreSignedGetUrl(String path, String fileName) {
+        String resourcePath = getEncodedResourcePath(path, fileName);
+        return createPreSignedUrl(resourcePath);
+    }
+
+    private String createPreSignedUrl(String resourcePath) {
         try {
-            String resourcePath = getEncodedResourcePath(path, fileName);
             String cloudFrontUrl = "https://" + distributionDomain + "/" + resourcePath;
             Instant expirationTime = Instant.now().plus(PRE_SIGNED_URL_EXPIRE_SECONDS, ChronoUnit.SECONDS);
             Path keyPath = Paths.get(privateKeyFilePath);
@@ -48,15 +65,19 @@ public class CloudFrontService {
                     .build();
             SignedUrl signedUrl = cloudFrontUtilities.getSignedUrlWithCannedPolicy(cannedSignerRequest);
             return signedUrl.url();
-        } catch(AWSBusinessException e){
+        } catch (AWSBusinessException e) {
             throw new AWSBusinessException(AWSExceptionType.GET_PRESIGNED_URL_AWS_CLOUDFRONT_FAIL);
         } catch (Exception e) {
             throw new AWSBusinessException(AWSExceptionType.NOT_FOUND_AWS_PRIVATE_KEY);
         }
     }
 
-    private String getEncodedResourcePath(String path, String fileName) throws UnsupportedEncodingException {
-        String encodedFileName = URLEncoder.encode(fileName, "UTF-8");
-        return path + encodedFileName;
+    private String getEncodedResourcePath(String path, String fileName) {
+        try {
+            String encodedFileName = URLEncoder.encode(fileName, "UTF-8");
+            return path + encodedFileName;
+        } catch (UnsupportedEncodingException e) {
+            throw new AWSBusinessException(AWSExceptionType.FAIL_ENCODING_FILE_NAME);
+        }
     }
 }
